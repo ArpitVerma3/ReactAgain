f48bb1bd64c287ee1cec19c9fd8d3314b0db9a97
ArpitVerma3 <varpit520@gmail.com>
2026-06-15 09:33:13 +0530

    todo1

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,17 @@
 //import { useState } from 'react'
 
+import AddTask from "./Components/AddTask";
+import AppName from "./Components/AppName";
+import TodoItem1 from "./Components/TodoItem1";
+import TodoItem2 from "./Components/todoItem2";
+
 function App() {
   return (
     <center class="todo-box">
-      <h1>Todo App</h1>
-
-      <div class="container text-center">
-        <div class="row">
-          <div class="col-6">
-            <input type="text" placeholder="Enter the tasks here"/>
-          </div>
-          <div class="col-4">
-            <input type="date"/>
-          </div>
-          <div class="col-2">
-            <button type="button" class="btn btn-success">Add</button>
-          </div>
-        </div>
-
-        <div class="row">
-          <div class="col-6">
-            <input type="text" placeholder="Enter the tasks here"/>
-          </div>
-          <div class="col-4">
-            <input type="date"/>
-          </div>
-          <div class="col-2">
-            <button type="button" class="btn btn-success">Add</button>
-          </div>
-          <button type="button" class="btn btn-danger">Delete</button>
-          </div>
-
-      </div>
+      <AppName></AppName>
+      <AddTask></AddTask>
+      <TodoItem1></TodoItem1>
+      <TodoItem2></TodoItem2>
     </center>
   );
 }
